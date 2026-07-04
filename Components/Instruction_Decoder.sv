ad04095753a89507a1286b4f95ceee560f38f7db
HamzaAndishmand <haandishmand@gmail.com>
2026-07-04 15:08:21 -0400

    Feat(Instruction_Decoder): Add base for R-type instruction

--- Components/Instruction_Decoder.sv
+++ Components/Instruction_Decoder.sv
@@ -1,8 +1,6 @@
-module Intstruction_Decoder(input [31:0] Intstruction,
-														output [4:0] rs1, rs2, rd,
-														output [5:0] op,
-														output [12:0] sim1m3);
-	
-	
+module Intstruction_Decoder(
+														input [31:0] Intstruction,
+														output [6:0] op);	
+
 
 endmodule
