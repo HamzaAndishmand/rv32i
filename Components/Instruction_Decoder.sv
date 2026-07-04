ac675e250e554d46b1312aa0deb2d0df6c53e71c
HamzaAndishmand <haandishmand@gmail.com>
2026-07-04 15:46:25 -0400

    Feat(Instruction_Decoder): Add R-type instruction parsing completed

--- Components/Instruction_Decoder.sv
+++ Components/Instruction_Decoder.sv
@@ -1,6 +1,20 @@
-module Intstruction_Decoder(
-														input [31:0] Intstruction,
-														output [6:0] op);	
+module Instruction_Decoder(
+		input [31:0] Intstruction,
+		output reg [6:0] op,
+		output reg [6:0] funct7,
+		output reg [2:0] funct3,
+		output reg [4:0] rs1,
+		output reg [4:0] rs2,
+		output reg [4:0] rd
+	);
 
+	always @(*) begin
+		op = Intstruction[6:0];
+		funct7 = Intstruction[31:25];
+		funct3 = Intstruction[14:12];
+		rs1 = Intstruction[19:15];
+		rs2 = Intstruction[24:20];
+		rd = Intstruction[11:7];
+	end 
 
 endmodule
