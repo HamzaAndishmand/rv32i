c08ad10e2ce473807358551f82802facfc178ee4
HamzaAndishmand <haandishmand@gmail.com>
2026-07-04 16:09:31 -0400

    Feat(Instruction_Decoder): Add I-Type op case
    refactor(Instruction_Decoder): Add case(op) for future implementation of other instruction types

--- Components/Instruction_Decoder.sv
+++ Components/Instruction_Decoder.sv
@@ -1,20 +1,32 @@
 module Instruction_Decoder(
-		input [31:0] Intstruction,
+		input [31:0] Instruction,
 		output reg [6:0] op,
 		output reg [6:0] funct7,
 		output reg [2:0] funct3,
 		output reg [4:0] rs1,
 		output reg [4:0] rs2,
-		output reg [4:0] rd
+		output reg [4:0] rd,
+		output reg [11:0] imm
 	);
 
 	always @(*) begin
-		op = Intstruction[6:0];
-		funct7 = Intstruction[31:25];
-		funct3 = Intstruction[14:12];
-		rs1 = Intstruction[19:15];
-		rs2 = Intstruction[24:20];
-		rd = Intstruction[11:7];
-	end 
-
+		op = Instruction[6:0];
+		case(op)
+			7'b0110011: begin //R-Type Instructions
+				funct7 = Instruction[31:25];
+				funct3 = Instruction[14:12];
+				rs1 = Instruction[19:15];
+				rs2 = Instruction[24:20];
+				rd = Instruction[11:7];
+			end
+			7'b0010011: begin //I-Type Instructions 
+				funct7 = 7'd0;
+				funct3 = Instruction[14:12];
+				rs1 = Instruction[19:15];
+				rs2 = 5'd0;
+				rd = Instruction[11:7];
+				imm = Instruction[31:20];
+			end
+		endcase
+	end
 endmodule
