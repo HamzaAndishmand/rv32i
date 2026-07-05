14c7e3589e540532ec27f4e92738290d45ff48b8
HamzaAndishmand <haandishmand@gmail.com>
2026-07-05 13:50:34 -0400

    Feat(ALU_Control): Add all R-Type functions implemented

--- Components/ALU_Control.sv
+++ Components/ALU_Control.sv
@@ -5,27 +5,42 @@ module ALU_Control(
 		
 	always @ (*) begin
 		case(opcode)
-			7'b0110011:
-				case(funct3)
+			7'b0110011: //R-Type Instructions
+				case(funct3) 
 					3'b000: begin
-						if (funct7 == 7'd0) begin
-							ALU_op = 4'b1100;
+						if (funct7 == 7'b0000000) begin 
+							ALU_op = 4'b1100; //SUB
 						end
 						else begin
-							ALU_op = 4'b1011;
+							ALU_op = 4'b1011; //ADD
 						end
 					end
 					3'b001: begin
-						ALU_op = 4'b0100;
+						ALU_op = 4'b0100; //SLL
 					end
 					3'b010: begin
+						ALU_op = 4'b1111; //SLT
 					end
 					3'b011: begin
+						ALU_op = 4'b1001; //SLTU
 					end 
 					3'b100: begin
+						ALU_op = 4'b0010; //XOR
 					end 
-					4'b101: begin
+					3'b101: begin
+						if (funct7 == 7'b0000000) begin
+							ALU_op = 4'b0101; //SRL
+						end 
+						else begin
+							ALU_op = 4'b0110; //SRA
+						end 
 					end
+					3'b110: begin
+						ALU_op = 4'b0001; //OR 
+					end
+					3'b111: begin
+						ALU_op = 4'b0000; //AND
+					end 
 				endcase
 		endcase
 	end
