6c3650063170d7a62542599cd1dd74cdbfdaa0ae
HamzaAndishmand <haandishmand@gmail.com>
2026-07-06 17:28:49 -0400

    Feat(ALU_Control) Add Basic I-Type Instructions

--- Components/ALU_Control.sv
+++ Components/ALU_Control.sv
@@ -1,6 +1,7 @@
 module ALU_Control(
 	input [6:0] opcode, funct7, 
 	input [2:0] funct3,
+	input [11:0] imm,
 	output logic [3:0] ALU_op);
 		
 	always_comb begin 
@@ -40,6 +41,39 @@ module ALU_Control(
 					end
 					3'b111: begin
 						ALU_op = 4'b0000; //AND
+					end
+				endcase
+
+			7'b0010011: //I-Type Instructions
+				case(funct3)
+					3'b000: begin
+						ALU_op = 4'b1011; //ADDI
+					end 
+					3'b001: begin
+						ALU_op = 4'b0100;
+					end 
+					3'b010: begin
+						ALU_op = 4'b1111;
+					end 
+					3'b011: begin 
+						ALU_op = 4'b1001;
+					end 
+					3'b100: begin
+						ALU_op = 4'b0010;
+					end 
+					3'b101: begin
+						if(imm[10] == 1) begin
+							ALU_op = 4'b0110;
+						end 
+						else begin
+							ALU_op = 4'b0101;
+						end
+					end 
+					3'b110: begin
+						ALU_op = 4'b0001;
+					end 
+					3'b111: begin
+						ALU_op = 4'b0000;
 					end 
 				endcase
 		endcase
