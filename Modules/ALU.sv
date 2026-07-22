d71411f09523b70646e1b006a11ae92aabcdf16a
HamzaAndishmand <haandishmand@gmail.com>
2026-07-21 22:20:03 -0400

    feat(alu): add opcode constants from isa_def_pkg to ALU
    ALU case statement can now stay in sync with future testbench and other modules that import the package

--- Modules/ALU.sv
+++ Modules/ALU.sv
@@ -1,3 +1,5 @@
+`include "isa_defs_pkg/defs_pkg.sv"
+import defs_pkg::*;
 module ALU(input [31:0] A, B,
 					input [3:0] control,
 					output logic [31:0] result,
@@ -6,38 +8,38 @@ module ALU(input [31:0] A, B,
 		status = 4'b0000;
 
 		case(control)
-			4'b0000:
-				result = A & B; //AND
-			4'b0001:
-				result = A | B; //OR
-			4'b0010:
-				result = A ^ B; //XOR
-			4'b0011:
-				result = ~A; //NOT
-			4'b0100:
-				result = A << B; //SLL
-			4'b0101:
-				result = A >> B; //SRL
-			4'b0110:
-				result = A >>> B; //SRA
-			4'b0111:
-				result = ~(A & B); //NAND
-			4'b1000:
-				result = ~(A ^ B); //XNOR
-			4'b1001:
-				result = (A < B) ? 32'd1 : 32'd0; //SLTU
-			4'b1010:
-				result = (A == B) ? 32'd1 : 32'd0; //Compare
-			4'b1011:
-				result = A + B; //ADD
-			4'b1100:
-				result = A - B; //SUB
-			4'b1101:
-				result = A * B; //Mult
-			4'b1110:
-				result = A / B; //Dev
-			4'b1111:
-				result = ($signed(A) < $signed(B)) ? 32'd1 : 32'd0; //SLT
+			AND:
+				result = A & B; 
+			OR:
+				result = A | B; 
+			XOR:
+				result = A ^ B; 
+			NOT:
+				result = ~A; 
+			SLL:
+				result = A << B; 
+			SRL:
+				result = A >> B; 
+			SRA:
+				result = A >>> B; 
+			NAND:
+				result = ~(A & B); 
+			XNOR:
+				result = ~(A ^ B);
+			SLTU:
+				result = (A < B) ? 32'd1 : 32'd0; 
+			SEQ:
+				result = (A == B) ? 32'd1 : 32'd0; 
+			ADD:
+				result = A + B; 
+			SUB:
+				result = A - B; 
+			MULT:
+				result = A * B; 
+			DEV:
+				result = A / B; 
+			SLT:
+				result = ($signed(A) < $signed(B)) ? 32'd1 : 32'd0; 
 			default result = 32'd0;
 		endcase
 	end
