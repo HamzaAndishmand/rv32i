4a16efd783a57cdfd54cc8b4ce990c1bf894c33d
HamzaAndishmand <haandishmand@gmail.com>
2026-07-22 01:28:04 -0400

    feat(alu_tb): add random ALU inputs and compute AND oracle inline

--- Testbench/ALU_tb.sv
+++ Testbench/ALU_tb.sv
@@ -1,27 +1,37 @@
-`include "Components/ALU.sv/"
+`include "Modules/ALU.sv"
 module ALU_tb();
-	logic [31:0] inputA, inputB;
+	logic [31:0] srcA, srcB;
 	logic [3:0] control;
 	logic [31:0] result;
 	logic [3:0] status;
+	logic [31:0] expected;
 
 	ALU dut(
-		.A(inputA),
-		.B(inputB),
+		.A(srcA),
+		.B(srcB),
 		.control(control),
 		.result(result),
 		.status(status)
 		);
 
 	initial begin
-		inputA = 12;
-		inputB = 10;
-		control = 0;
-		#5
-		
-		$display("Result: %d", result);
-	$finish;
+
+		control = AND;
+
+		srcA[31:0] = $urandom;
+		srcB[31:0] = $urandom;
+		#10;
+
+		$display("Control is: %d", control);
+		expected = srcA & srcB;
+
+		if (expected !== result) begin
+			$error("Failed at srcA=%d, srcB=%d, and control=%d", srcA, srcB, control);
+		end
+		else begin
+			$display("Passed");
+		end
+		$display("Your answer was: %d", expected);	
+		$display("Real answer was: %d", result);
 	end
 endmodule
-	
-
