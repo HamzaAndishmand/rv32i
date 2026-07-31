a61aaf4f7a6d34478e8ff86700bf482309295e9f
HamzaAndishmand <haandishmand@gmail.com>
2026-07-31 05:29:43 -0400

    refactor(alu_control): add opcode constants to ALU_Control.sv for future syncing

--- Modules/ALU_Control.sv
+++ Modules/ALU_Control.sv
@@ -1,3 +1,4 @@
+import defs_pkg::*;
 module ALU_Control(
 	input [6:0] opcode, funct7, 
 	input [2:0] funct3,
@@ -10,70 +11,70 @@ module ALU_Control(
 				case(funct3) 
 					3'b000: begin
 						if (funct7 == 7'b0000000) begin 
-							ALU_op = 4'b1011; //SUB
+							ALU_op = SUB;
 						end
 						else begin
-							ALU_op = 4'b1100; //ADD
+							ALU_op = ADD;
 						end
 					end
 					3'b001: begin
-						ALU_op = 4'b0100; //SLL
+						ALU_op = SLL; 
 					end
 					3'b010: begin
-						ALU_op = 4'b1111; //SLT
+						ALU_op = SLT; 
 					end
 					3'b011: begin
-						ALU_op = 4'b1001; //SLTU
+						ALU_op = SLTU; 
 					end 
 					3'b100: begin
-						ALU_op = 4'b0010; //XOR
+						ALU_op = XOR; 
 					end 
 					3'b101: begin
 						if (funct7 == 7'b0000000) begin
-							ALU_op = 4'b0101; //SRL
+							ALU_op = SRL; 
 						end 
 						else begin
-							ALU_op = 4'b0110; //SRA
+							ALU_op = SRA; 
 						end 
 					end
 					3'b110: begin
-						ALU_op = 4'b0001; //OR 
+						ALU_op = OR; 
 					end
 					3'b111: begin
-						ALU_op = 4'b0000; //AND
+						ALU_op = AND; 
 					end
 				endcase
 
 			7'b0010011: //I-Type Instructions
 				case(funct3)
 					3'b000: begin
-						ALU_op = 4'b1011; //ADDI
+						ALU_op = ADD; 
 					end 
 					3'b001: begin
-						ALU_op = 4'b0100;
+						ALU_op = SLL;
 					end 
 					3'b010: begin
-						ALU_op = 4'b1111;
+						ALU_op = SLT;
 					end 
 					3'b011: begin 
-						ALU_op = 4'b1001;
+						ALU_op = SLTU;
 					end 
 					3'b100: begin
-						ALU_op = 4'b0010;
+						ALU_op = XOR;
 					end 
 					3'b101: begin
 						if(imm[10] == 1) begin
-							ALU_op = 4'b0110;
+							ALU_op = SRA;
 						end 
 						else begin
-							ALU_op = 4'b0101;
+							ALU_op = SRL;
 						end
 					end 
 					3'b110: begin
-						ALU_op = 4'b0001;
+						ALU_op = OR;
 					end 
 					3'b111: begin
-						ALU_op = 4'b0000;
+						ALU_op = AND;
 					end 
 				endcase
 		endcase
